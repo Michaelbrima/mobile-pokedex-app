fbe78cb6e459978d2f7f6a6d744bd10223baf246
Michaelbrima <157252258+Michaelbrima@users.noreply.github.com>
2025-04-01 08:56:58 -0400

    multiple entries can now be featured in a vertical list that can be scrolled

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React,{ useEffect, useState } from 'react';
-import { Text, View , StyleSheet, FlatList, ListRenderItem, Platform, Image } from 'react-native'; /*'Stylesheet' is a property that allows you to use CSS properties in React Native. */ 
+import { Text, View , StyleSheet, FlatList, ListRenderItem, Platform, Image, ScrollView } from 'react-native'; /*'Stylesheet' is a property that allows you to use CSS properties in React Native. */ 
 import axios from 'axios';
 // const pokemonEntry = ({ pokemon }) => (
 //     <View>
@@ -8,7 +8,7 @@ import axios from 'axios';
 // )
 
 export default function Index() {
-    const [record, setRecord] = useState<any>({});
+    // const [record, setRecord] = useState<any>({});
     const [bulbasaur, setBulbasaur] = useState<any>({});
     const [squirtle, setSquirtle] = useState<any>({});
     const [pidgey, setPidgey] = useState<any>({});
@@ -35,9 +35,9 @@ export default function Index() {
     //     }
     // };
     
-    useEffect(() => {
-        getPData();
-    }, []);
+    // useEffect(() => {
+    //     getPData();
+    // }, []);
 
     useEffect(() => {
         Promise.all([
@@ -52,35 +52,36 @@ export default function Index() {
         setBulbasaur(dataBulbasaur);
         setSquirtle(dataSquirtle);
         setPidgey(dataPidgey);
+        setLoading(false);
     });
     }, []);
 
-    const getPData = async () => {
-        const URL = "https://pokeapi.co/api/v2/pokemon/charmander";
-        // fetch(URL)
-        // .then((res) => {
-        //     return res.json(); //convert json into a readable format (or have it parsed)
-        // })
-        // .then((responseData) => {
-        //     // console.log(responseData);
-        //     setRecord(responseData);
-        // });
-        setLoading(true);
-        try{
-            const response = await axios.get(URL);
-            const data = response.data;
-            setRecord(data);
-            console.log(data);
-        } catch (error: any ) {
-            console.log(error);
-        } finally{
-            setLoading(false);
-        }
+    // const getPData = async () => {
+    //     const URL = "https://pokeapi.co/api/v2/pokemon/charmander";
+    //     // fetch(URL)
+    //     // .then((res) => {
+    //     //     return res.json(); //convert json into a readable format (or have it parsed)
+    //     // })
+    //     // .then((responseData) => {
+    //     //     // console.log(responseData);
+    //     //     setRecord(responseData);
+    //     // });
+    //     setLoading(true);
+    //     try{
+    //         const response = await axios.get(URL);
+    //         const data = response.data;
+    //         setRecord(data);
+    //         console.log(data);
+    //     } catch (error: any ) {
+    //         console.log(error);
+    //     } finally{
+    //         setLoading(false);
+    //     }
 
-        // const resp = await fetch(URL);
-        // const json = await resp.json();
-        // console.log(json);
-    };
+    //     // const resp = await fetch(URL);
+    //     // const json = await resp.json();
+    //     // console.log(json);
+    // };
 
     // useEffect(() => {
     // fetchPData();
@@ -92,12 +93,23 @@ export default function Index() {
     if (loading) return <Text>Loading...</Text>;
 
     return (
+    <ScrollView>
         <View style={styles.container}>
+            
+            <Image source={{ uri: bulbasaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+            <Text style={styles.text}>{bulbasaur.name}</Text>
+            <Image source={{ uri: squirtle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+            <Text style={styles.text}>{squirtle.name}</Text>
+            <Image source={{ uri: pidgey.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+            <Text style={styles.text}>{pidgey.name}</Text>
+        </View>
+        <View style={styles.container}>
+            
             {/* <Text style={styles.text}>Hello Expo.</Text>
             <Text style={styles.text}>This is the mobile-pokedex-app.</Text> */}
             {/* <Text style={styles.text}>{record.name}</Text> */}
-            <Image source={{ uri: record.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-            <Text style={styles.text}>{record.name}</Text>
+            {/* <Image source={{ uri: record.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+            <Text style={styles.text}>{record.name}</Text> */}
             {/* <Text style={styles.text}>{record.moves.move.name}</Text> */}
             {/* <Text style={styles.text}>Index</Text> */}
             {/* <FlatList data={record} renderItem={({pokemon})=> <Pokemon data={pokemon} />}/> */}
@@ -111,6 +123,55 @@ export default function Index() {
             <Image source={{ uri: pidgey.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
             <Text style={styles.text}>{pidgey.name}</Text>
         </View>
+        <View>
+        <Image source={{ uri: bulbasaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{bulbasaur.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: squirtle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{squirtle.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: pidgey.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{pidgey.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: bulbasaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{bulbasaur.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: squirtle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{squirtle.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: pidgey.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{pidgey.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: bulbasaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{bulbasaur.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: squirtle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{squirtle.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: pidgey.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{pidgey.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: bulbasaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{bulbasaur.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: squirtle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{squirtle.name}</Text>
+        </View>
+        <View>
+        <Image source={{ uri: pidgey.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+        <Text style={styles.text}>{pidgey.name}</Text>
+        </View>
+    </ScrollView>
     );
     
 };
