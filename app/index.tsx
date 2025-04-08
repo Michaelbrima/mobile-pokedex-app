bcbaca8e73a475eea5d689747a17cb86aa0049c9
Michaelbrima <157252258+Michaelbrima@users.noreply.github.com>
2025-04-08 08:05:11 -0400

    added moveset or moves that can be learned

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import React,{ useEffect, useState } from 'react';
 import { Text, View , StyleSheet, FlatList, ListRenderItem, Platform, Image, ScrollView, Pressable } from 'react-native'; /*'Stylesheet' is a property that allows you to use CSS properties in React Native. */ 
 import axios from 'axios';
+import { SafeAreaView } from 'react-native-safe-area-context';
 // const pokemonEntry = ({ pokemon }) => (
 //     <View>
 //         <Text>{pokemon.name}</Text>
@@ -133,127 +134,196 @@ export default function Index() {
     });
     }, []);
 
+    const data = [
+        bulbasaur,
+        ivysaur,
+        venusaur,
+        charmander,
+        charmeleon,
+        charizard,
+        squirtle,
+        wartortle,
+        blastoise,
+        caterpie,
+        metapod,
+        butterfree,
+        weedle,
+        kakuna,
+        beedrill,
+        pidgey,
+        pidgeotto,
+        pidgeot
+    ];
+
 //     const bulbasaurType = () => {
 //     if (loading) return <Text>Loading type...</Text>
 //     return (bulbasaur.types.type.name);
 // }
+
+// const Move = ({move}: { move: any }) => (
+//     <View>
+//         <Text> {move}</Text>
+//     </View>
+// );
+
+// const renderItem = ({item}: { item: any }) => {
+//     return (<View>
+//         <Text>{item.move.name}</Text>
+//     </View>); 
+// };
+
+const renderItem = ({ item }: { item: any }) => {
+    //the app will represent each list item via a Text component
+    return <Text> {item.title}</Text>;
+  };
  
     if (loading) return <Text>Loading...</Text>;
 
     return (
-    <ScrollView>
-        <View style={styles.container}>
-            
-            <Image source={{ uri: bulbasaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-            <Text style={styles.text}>{bulbasaur.name}</Text>
-            <Image source={{ uri: squirtle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-            <Text style={styles.text}>{squirtle.name}</Text>
-            <Image source={{ uri: pidgey.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-            <Text style={styles.text}>{pidgey.name}</Text>
-        </View>
-        <View style={styles.container}>
-            
-            {/* <Text style={styles.text}>Hello Expo.</Text>
-            <Text style={styles.text}>This is the mobile-pokedex-app.</Text> */}
-            {/* <Text style={styles.text}>{record.name}</Text> */}
-            {/* <Image source={{ uri: record.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-            <Text style={styles.text}>{record.name}</Text> */}
-            {/* <Text style={styles.text}>{record.moves.move.name}</Text> */}
-            {/* <Text style={styles.text}>Index</Text> */}
-            {/* <FlatList data={record} renderItem={({pokemon})=> <Pokemon data={pokemon} />}/> */}
-           
-            
-            {/* <Text style={styles.text}>{info.data}</Text> */}
-            <Image source={{ uri: bulbasaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-            <Text style={styles.text}>{bulbasaur.name}</Text>
-            <Image source={{ uri: squirtle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-            <Text style={styles.text}>{squirtle.name}</Text>
-            <Image source={{ uri: pidgey.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-            <Text style={styles.text}>{pidgey.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Pressable onPress={() => setExpanded(!expanded)}>
-        <Image source={{ uri: bulbasaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{bulbasaur.name}</Text>
-        <Text style={styles.text}>{bulbasaur.name}</Text>
-        </Pressable>
-        {expanded && <View style={styles.textWrapper}>
+        // <>
+        <SafeAreaView>
+            <ScrollView>
+                <View>
+                        <View>
+                            {bulbasaur.moves.map((item: { id: React.Key | null | undefined; move: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }; }, i: any) => //used typescript to help infer parameter types from usage for both props
+                                <View key={i}>
+                                    {<Text >{item.move.name}</Text>}
+                                </View>)}
+                        </View>
+                    </View>
+                <View style={styles.container}>
+
+                    <Image source={{ uri: bulbasaur.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{bulbasaur.name}</Text>
+                    <Image source={{ uri: squirtle.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{squirtle.name}</Text>
+                    <Image source={{ uri: pidgey.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{pidgey.name}</Text>
+                </View>
+                <View style={styles.container}>
+
+                    {/* <Text style={styles.text}>Hello Expo.</Text>
+    <Text style={styles.text}>This is the mobile-pokedex-app.</Text> */}
+                    {/* <Text style={styles.text}>{record.name}</Text> */}
+                    {/* <Image source={{ uri: record.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
+    <Text style={styles.text}>{record.name}</Text> */}
+                    {/* <Text style={styles.text}>{record.moves.move.name}</Text> */}
+                    {/* <Text style={styles.text}>Index</Text> */}
+                    {/* <FlatList data={record} renderItem={({pokemon})=> <Pokemon data={pokemon} />}/> */}
+
+
+                    {/* <Text style={styles.text}>{info.data}</Text> */}
+                    <Image source={{ uri: bulbasaur.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{bulbasaur.name}</Text>
+                    <Image source={{ uri: squirtle.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{squirtle.name}</Text>
+                    <Image source={{ uri: pidgey.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{pidgey.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Pressable onPress={() => setExpanded(!expanded)}>
+                        <Image source={{ uri: bulbasaur.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                        <Text style={styles.text}>{bulbasaur.name}</Text>
+                        <Text style={styles.text}>{bulbasaur.name}</Text>
+                    </Pressable>
+                    {expanded && <View style={styles.textWrapper}>
                         <Text style={styles.text}>{bulbasaur.types[0].type.name}</Text>
                         <Text style={styles.text}>{bulbasaur.abilities[0].ability.name}</Text>
                     </View>}
-        
-        {expanded && <Text> </Text>}
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: ivysaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{ivysaur.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: venusaur.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{venusaur.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: charmander.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{charmander.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: charmeleon.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{charmeleon.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: charizard.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{charizard.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: squirtle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{squirtle.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: wartortle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{wartortle.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: blastoise.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{blastoise.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: caterpie.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{caterpie.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: metapod.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{metapod.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: butterfree.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{butterfree.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: weedle.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{weedle.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: kakuna.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{kakuna.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: beedrill.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{beedrill.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: pidgey.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{pidgey.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: pidgeotto.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{pidgeotto.name}</Text>
-        </View>
-        <View style={styles.container}>
-        <Image source={{ uri: pidgeot.sprites.front_default}} style={{ width: 50, height: 50, borderRadius: 100/2, }} />
-        <Text style={styles.text}>{pidgeot.name}</Text>
-        </View>
-    </ScrollView>
-    );
+
+                    {expanded && <Text> </Text>}
+                    {bulbasaur && expanded &&
+                        // &&
+                        //     && (
+                        //     <FlatList
+                        //       data={bulbasaur.moves}
+                        //       keyExtractor={(item)=> item.move.name}
+                        //       renderItem={renderItem}
+                        //     />
+                        //   )
+                        //  bulbasaur.moves.map((item: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined)=> <Text>{item}</Text>)
+                    <Text>bulbasaur.moves[0].move.name</Text>
+                    
+                    
+                    }
+                        {/* <View>{bulbasaur.moves.map((item: any) => <View>
+                            {<Text>{item}</Text>}
+                        </View>)}</View> */}
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: ivysaur.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{ivysaur.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: venusaur.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{venusaur.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: charmander.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{charmander.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: charmeleon.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{charmeleon.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: charizard.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{charizard.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: squirtle.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{squirtle.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: wartortle.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{wartortle.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: blastoise.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{blastoise.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: caterpie.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{caterpie.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: metapod.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{metapod.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: butterfree.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{butterfree.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: weedle.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{weedle.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: kakuna.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{kakuna.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: beedrill.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{beedrill.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: pidgey.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{pidgey.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: pidgeotto.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{pidgeotto.name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image source={{ uri: pidgeot.sprites.front_default }} style={{ width: 50, height: 50, borderRadius: 100 / 2, }} />
+                    <Text style={styles.text}>{pidgeot.name}</Text>
+                </View>
+            </ScrollView>
+
+        </SafeAreaView>
+//         <FlatList data={data} keyExtractor={(item) => item.id} renderItem={renderItem} />
+//    </>
+);
     
 };
 
