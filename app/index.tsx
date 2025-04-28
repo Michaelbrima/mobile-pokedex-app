970317e9799dda9017ff2433189b96236db7089d
Michaelbrima <157252258+Michaelbrima@users.noreply.github.com>
2025-04-28 08:33:45 -0400

    added more entries from abra through marowak

--- app/index.tsx
+++ app/index.tsx
@@ -72,6 +72,50 @@ export default function Index(this: any) {
     const [poliwag, setPoliwag] = useState<any>({});
     const [poliwhirl, setPoliwhirl] = useState<any>({});
     const [poliwrath, setPoliwrath] = useState<any>({});
+    const [abra, setAbra] = useState<any>({});
+    const [kadabra, setKadabra] = useState<any>({});
+    const [alakazam, setAlakazam] = useState<any>({});
+    const [machop, setMachop] = useState<any>({});
+    const [machoke, setMachoke] = useState<any>({});
+    const [machamp, setMachamp] = useState<any>({});
+    const [bellsprout, setBellsprout] = useState<any>({});
+    const [weepinbell, setWeepinbell] = useState<any>({});
+    const [victreebel, setVictreebel] = useState<any>({});
+    const [tentacool, setTentacool] = useState<any>({});
+    const [tentacruel, setTentacruel] = useState<any>({});
+    const [geodude, setGeodude] = useState<any>({});
+    const [graveler, setGraveler] = useState<any>({});
+    const [golem, setGolem] = useState<any>({});
+    const [ponyta, setPonyta] = useState<any>({});
+    const [rapidash, setRapidash] = useState<any>({});
+    const [slowpoke, setSlowpoke] = useState<any>({});
+    const [slowbro, setSlowbro] = useState<any>({});
+    const [magnemite, setMagnemite] = useState<any>({});
+    const [magneton, setMagneton] = useState<any>({});
+    const [farfetchd, setFarfetchd] = useState<any>({});
+    const [doduo, setDoduo] = useState<any>({});
+    const [dodrio, setDodrio] = useState<any>({});
+    const [seel, setSeel] = useState<any>({});
+    const [dewgong, setDewgong] = useState<any>({});
+    const [grimer, setGrimer] = useState<any>({});
+    const [muk, setMuk] = useState<any>({});
+    const [shellder, setShellder] = useState<any>({});
+    const [cloyster, setCloyster] = useState<any>({});
+    const [gastly, setGastly] = useState<any>({});
+    const [haunter, setHaunter] = useState<any>({});
+    const [gengar, setGengar] = useState<any>({});
+    const [onix, setOnix] = useState<any>({});
+    const [drowzee, setDrowzee] = useState<any>({});
+    const [hypno, setHypno] = useState<any>({});
+    const [krabby, setKrabby] = useState<any>({});
+    const [kingler, setKingler] = useState<any>({});
+    const [voltorb, setVoltorb] = useState<any>({});
+    const [electrode, setElectrode] = useState<any>({});
+    const [exeggcute, setExeggcute] = useState<any>({});
+    const [exeggutor, setExeggutor] = useState<any>({});
+    const [cubone, setCubone] = useState<any>({});
+    const [marowak, setMarowak] = useState<any>({});
+
 
     const [locationBulbasaur, setLocationBulbasaur] = useState<any>({});
     const [areaBulbasaur, setAreaBulbasaur] = useState<any>([]);
@@ -146,6 +190,49 @@ export default function Index(this: any) {
             fetch('https://pokeapi.co/api/v2/pokemon/poliwag'),
             fetch('https://pokeapi.co/api/v2/pokemon/poliwhirl'),
             fetch('https://pokeapi.co/api/v2/pokemon/poliwrath'),
+            fetch('https://pokeapi.co/api/v2/pokemon/abra'),
+            fetch('https://pokeapi.co/api/v2/pokemon/kadabra'),
+            fetch('https://pokeapi.co/api/v2/pokemon/Alakazam'),
+            fetch('https://pokeapi.co/api/v2/pokemon/machop'),
+            fetch('https://pokeapi.co/api/v2/pokemon/machoke'),
+            fetch('https://pokeapi.co/api/v2/pokemon/machamp'),
+            fetch('https://pokeapi.co/api/v2/pokemon/bellsprout'),
+            fetch('https://pokeapi.co/api/v2/pokemon/weepinbell'),
+            fetch('https://pokeapi.co/api/v2/pokemon/victreebel'),
+            fetch('https://pokeapi.co/api/v2/pokemon/tentacool'),
+            fetch('https://pokeapi.co/api/v2/pokemon/tentacruel'),
+            fetch('https://pokeapi.co/api/v2/pokemon/geodude'),
+            fetch('https://pokeapi.co/api/v2/pokemon/graveler'),
+            fetch('https://pokeapi.co/api/v2/pokemon/golem'),
+            fetch('https://pokeapi.co/api/v2/pokemon/ponyta'),
+            fetch('https://pokeapi.co/api/v2/pokemon/rapidash'),
+            fetch('https://pokeapi.co/api/v2/pokemon/slowpoke'),
+            fetch('https://pokeapi.co/api/v2/pokemon/slowbro'),
+            fetch('https://pokeapi.co/api/v2/pokemon/magnemite'),
+            fetch('https://pokeapi.co/api/v2/pokemon/magneton'),
+            fetch('https://pokeapi.co/api/v2/pokemon/farfetchd'),
+            fetch('https://pokeapi.co/api/v2/pokemon/doduo'),
+            fetch('https://pokeapi.co/api/v2/pokemon/dodrio'),
+            fetch('https://pokeapi.co/api/v2/pokemon/seel'),
+            fetch('https://pokeapi.co/api/v2/pokemon/dewgong'),
+            fetch('https://pokeapi.co/api/v2/pokemon/grimer'),
+            fetch('https://pokeapi.co/api/v2/pokemon/muk'),
+            fetch('https://pokeapi.co/api/v2/pokemon/shellder'),
+            fetch('https://pokeapi.co/api/v2/pokemon/cloyster'),
+            fetch('https://pokeapi.co/api/v2/pokemon/gastly'),
+            fetch('https://pokeapi.co/api/v2/pokemon/haunter'),
+            fetch('https://pokeapi.co/api/v2/pokemon/gengar'),
+            fetch('https://pokeapi.co/api/v2/pokemon/onix'),
+            fetch('https://pokeapi.co/api/v2/pokemon/drowzee'),
+            fetch('https://pokeapi.co/api/v2/pokemon/hypno'),
+            fetch('https://pokeapi.co/api/v2/pokemon/krabby'),
+            fetch('https://pokeapi.co/api/v2/pokemon/kingler'),
+            fetch('https://pokeapi.co/api/v2/pokemon/voltorb'),
+            fetch('https://pokeapi.co/api/v2/pokemon/electrode'),
+            fetch('https://pokeapi.co/api/v2/pokemon/exeggcute'),
+            fetch('https://pokeapi.co/api/v2/pokemon/exeggutor'),
+            fetch('https://pokeapi.co/api/v2/pokemon/cubone'),
+            fetch('https://pokeapi.co/api/v2/pokemon/marowak'),
             fetch('https://pokeapi.co/api/v2/pokemon/1/encounters')
         ])
         .then(([resBulbasaur, 
@@ -210,6 +297,49 @@ export default function Index(this: any) {
             resPoliwag,
             resPoliwhirl,
             resPoliwrath,
+            resAbra,
+            resKadabra,
+            resAlakazam,
+            resMachop,
+            resMachoke,
+            resMachamp,
+            resBellsprout,
+            resWeepinbell,
+            resVictreebel,
+            resTentacool,
+            resTentacruel,
+            resGeodude,
+            resGraveler,
+            resGolem,
+            resPonyta,
+            resRapidash,
+            resSlowpoke,
+            resSlowbro,
+            resMagnemite,
+            resMagneton,
+            resFarfetchd,
+            resDoduo,
+            resDodrio,
+            resSeel,
+            resDewgong,
+            resGrimer,
+            resMuk,
+            resShellder,
+            resCloyster,
+            resGastly,
+            resHaunter,
+            resGengar,
+            resOnix,
+            resDrowzee,
+            resHypno,
+            resKrabby,
+            resKingler,
+            resVoltorb,
+            resElectrode,
+            resExeggcute,
+            resExeggutor,
+            resCubone,
+            resMarowak,
             resLocationBulbasaur
         ]) =>
         Promise.all([resBulbasaur.json(), 
@@ -274,6 +404,49 @@ export default function Index(this: any) {
             resPoliwag.json(),
             resPoliwhirl.json(),
             resPoliwrath.json(),
+            resAbra.json(),
+            resKadabra.json(),
+            resAlakazam.json(),
+            resMachop.json(),
+            resMachoke.json(),
+            resMachamp.json(),
+            resBellsprout.json(),
+            resWeepinbell.json(),
+            resVictreebel.json(),
+            resTentacool.json(),
+            resTentacruel.json(),
+            resGeodude.json(),
+            resGraveler.json(),
+            resGolem.json(),
+            resPonyta.json(),
+            resRapidash.json(),
+            resSlowpoke.json(),
+            resSlowbro.json(),
+            resMagnemite.json(),
+            resMagneton.json(),
+            resFarfetchd.json(),
+            resDoduo.json(),
+            resDodrio.json(),
+            resSeel.json(),
+            resDewgong.json(),
+            resGrimer.json(),
+            resMuk.json(),
+            resShellder.json(),
+            resCloyster.json(),
+            resGastly.json(),
+            resHaunter.json(),
+            resGengar.json(),
+            resOnix.json(),
+            resDrowzee.json(),
+            resHypno.json(),
+            resKrabby.json(),
+            resKingler.json(),
+            resVoltorb.json(),
+            resElectrode.json(),
+            resExeggcute.json(),
+            resExeggutor.json(),
+            resCubone.json(),
+            resMarowak.json(),
             resLocationBulbasaur.json()])
     )
     .then(([dataBulbasaur, 
@@ -338,6 +511,49 @@ export default function Index(this: any) {
         dataPoliwag,
         dataPoliwhirl,
         dataPoliwrath,
+        dataAbra,
+        dataKadabra,
+        dataAlakazam,
+        dataMachop,
+        dataMachoke,
+        dataMachamp,
+        dataBellsprout,
+        dataWeepinbell,
+        dataVictreebel,
+        dataTentacool,
+        dataTentacruel,
+        dataGeodude,
+        dataGraveler,
+        dataGolem,
+        dataPonyta,
+        dataRapidash,
+        dataSlowpoke,
+        dataSlowbro,
+        dataMagnemite,
+        dataMagneton,
+        dataFarfetchd,
+        dataDoduo,
+        dataDodrio,
+        dataSeel,
+        dataDewgong,
+        dataGrimer,
+        dataMuk,
+        dataShellder,
+        dataCloyster,
+        dataGastly,
+        dataHaunter,
+        dataGengar,
+        dataOnix,
+        dataDrowzee,
+        dataHypno,
+        dataKrabby,
+        dataKingler,
+        dataVoltorb,
+        dataElectrode,
+        dataExeggcute,
+        dataExeggutor,
+        dataCubone,
+        dataMarowak,
         dataLocationBulbasaur]) => {
         setBulbasaur(dataBulbasaur);
         setIvysaur(dataIvysaur);
@@ -401,6 +617,49 @@ export default function Index(this: any) {
         setPoliwag(dataPoliwag);
         setPoliwhirl(dataPoliwhirl);
         setPoliwrath(dataPoliwrath);
+        setAbra(dataAbra);
+        setKadabra(dataKadabra);
+        setAlakazam(dataAlakazam);
+        setMachop(dataMachop);
+        setMachoke(dataMachoke);
+        setMachamp(dataMachamp);
+        setBellsprout(dataBellsprout);
+        setWeepinbell(dataWeepinbell);
+        setVictreebel(dataVictreebel);
+        setTentacool(dataTentacool);
+        setTentacruel(dataTentacruel);
+        setGeodude(dataGeodude);
+        setGraveler(dataGraveler);
+        setGolem(dataGolem);
+        setPonyta(dataPonyta);
+        setRapidash(dataRapidash);
+        setSlowpoke(dataSlowpoke);
+        setSlowbro(dataSlowbro);
+        setMagnemite(dataMagnemite);
+        setMagneton(dataMagneton);
+        setFarfetchd(dataFarfetchd);
+        setDoduo(dataDoduo);
+        setDodrio(dataDodrio);
+        setSeel(dataSeel);
+        setDewgong(dataDewgong);
+        setGrimer(dataGrimer);
+        setMuk(dataMuk);
+        setShellder(dataShellder);
+        setCloyster(dataCloyster);
+        setGastly(dataGastly);
+        setHaunter(dataHaunter);
+        setGengar(dataGengar);
+        setOnix(dataOnix);
+        setDrowzee(dataDrowzee);
+        setHypno(dataHypno);
+        setKrabby(dataKrabby);
+        setKingler(dataKingler);
+        setVoltorb(dataVoltorb);
+        setElectrode(dataElectrode);
+        setExeggcute(dataExeggcute);
+        setExeggutor(dataExeggutor);
+        setCubone(dataCubone);
+        setMarowak(dataMarowak);
         setLocationBulbasaur(dataLocationBulbasaur);
         setLoading(false);
     });
@@ -492,7 +751,50 @@ export default function Index(this: any) {
         arcanine,
         poliwag,
         poliwhirl,
-        poliwrath
+        poliwrath,
+        abra,
+        kadabra,
+        alakazam,
+        machop,
+        machoke,
+        machamp,
+        bellsprout,
+        weepinbell,
+        victreebel,
+        tentacool,
+        tentacruel,
+        geodude,
+        graveler,
+        golem,
+        ponyta,
+        rapidash,
+        slowpoke,
+        slowbro,
+        magnemite,
+        magneton,
+        farfetchd,
+        doduo,
+        dodrio,
+        seel,
+        dewgong,
+        grimer,
+        muk,
+        shellder,
+        cloyster,
+        gastly,
+        haunter,
+        gengar,
+        onix,
+        drowzee,
+        hypno,
+        krabby,
+        kingler,
+        voltorb,
+        electrode,
+        exeggcute,
+        exeggutor,
+        cubone,
+        marowak
     ];
 
 
